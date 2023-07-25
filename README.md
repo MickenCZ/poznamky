bab845fcd608e088b20a8789a502ac42df4dcf1c
MickenCZ <MickenCZ@seznam.cz>
2023-07-25 15:49:57 +0200

    asd

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ server na poznamky
 1. Vytvořím složku public (pojmenuj si to jak chceš) a z ní budu brát všechny statické html soubory. To dělá to ```express.static```
 1. Vytvořím v ní všechny potřebné soubory. Prozatím stačí index.html, register.html a login.html. Do této samé složky dávám i CSS případně JS a obrázky.
 1. Pro všechny tři vytvořím endpointy tak, aby poslali příslušný soubor. Priklad.com/register je registrace, priklad.com/login je přihlášení atd, prohlížení poznamek je na priklad.com, tedy na index.html
+
+## Commit 2
+1. Vytvořím formulář na registraci a login, včetně CSS. Důležité je dát form tagu atribut method POST a action /register, tedy jméno endpointu na zpracování POST requestu po registraci. To samé pro login. Tady dej pozor na to, že by měli být na inputech správné name attributy. Podle nich pak budeš brát informace na serveru z HTTP request body. Dej také pozor na správné parsování dat z formulářů, zde používám ```app.use(express.urlencoded({ extended: true }));```
+1.  Máme tedy udělané graficky formuláře, method, action, name na inputech, css ve složce a na serveru přijímání údajů na přihlášení.
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 app.use(express.static(path.join(__dirname, 'public'))) //ber soubory ze složky public
+app.use(express.urlencoded({ extended: true })); //parsuj json data z formulářů
 
 //po get requestu na / pošli zpátky index.html
 app.get('/', (req, res) => {
@@ -15,6 +16,41 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'))
 });
+
+
+
+
+
+
+
+app.post('/register', (req, res) => {
+    const { username, email, password } = req.body;
+  
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: 'Pro registraci je nutné mít jméno, email a heslo' });
+    }
+  
+    console.log('Name:', username);
+    console.log('Email:', email);
+    console.log('Password:', password);
+  
+    res.status(200).send() //zde bude později redirect na index
+  });
+
+
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Pro přihlášení je nutné mít jméno a heslo' });
+    }
+  
+    console.log('Name:', username);
+    console.log('Password:', password);
+  
+    res.status(200).send() //zde bude později redirect na index
+  });
+
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`)
